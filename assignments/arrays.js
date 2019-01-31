5c245b9356f848a4aebdd499a2825a2293bd3ced
jackgipson <jackjgipson@gmail.com>
2019-01-30 20:15:00 -0800

    arrays challenge 4 answered with map

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -90,6 +90,14 @@ for (let i = 0; i < inventory.length; i++) {
   
   console.log(carYears);
 
+  // Using Map
+
+  let carYears = inventory.map(function(num) {
+    return num.car_year;
+  });
+  
+  console.log(carYears);
+
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 // let oldCars =[];
